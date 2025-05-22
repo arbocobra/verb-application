fb3fd14aab91f970634b89766386c70901359e11
Natalie Rekai <ncrekai@gmail.com>
2025-05-22 18:22:32 -0400

    api url Front

--- client/src/components/TestContainer.jsx
+++ client/src/components/TestContainer.jsx
@@ -31,7 +31,7 @@ const TestContainer = (props) => {
    }, [])
 
    const loadData = () => {
-      fetch(process.env.VITE_API_URL)
+      fetch(`${process.env.VITE_API_URL}/api/verbs`)
       .then(response => {
          if (!response.ok) { throw new Error(`HTTP error! Status: ${response.status}`); }
          return response.json();
