53b88dfc36adddea1f78c61bfffabd6dda411017
Natalie Rekai <ncrekai@gmail.com>
2025-05-25 12:31:40 -0400

    clear backend code

--- server/db/routesHints.js
+++ server/db/routesHints.js
@@ -2,8 +2,6 @@ import express from 'express';
 import { getHintsByTense } from './controllersHints.js';
 
 const router = express.Router();
-
-// router.get('/', getAllVerbs);
 router.get('/:tense', getHintsByTense);
 
 export default router;
--- server/server.js
+++ server/server.js
@@ -1,23 +1,14 @@
-import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import cors from 'cors';
-import verbRoutes from './db/routesVerb.js';
 import app from './express.js'
 
 dotenv.config();
 
-// const app = express();
 const port = process.env.PORT;
-// app.use(cors());
-// app.use(express.json());
-
 
 mongoose.connect(process.env.MONGO_URI)
    .then(() => console.log('Connected to MongoDB'))
    .catch((err) => console.error(err));
-
-// app.use('/verbs', verbRoutes);
 
 app.listen(port, () => {
    console.log(`Server is running on ${port}`);
